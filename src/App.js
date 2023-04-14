65be46118a49d2025a3a9dde6c52d5ea5df839e8
topilogy <topilogy@gmail.com>
2023-04-14 12:46:00 +0100

    Deleting Task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import './App.css';
 import { useState } from "react"
 
+//Deleting Task
+
 function App() {
-  const value = localStorage.getItem('num')
-  const [number, setNumber] = useState(value ? JSON.parse(value) : 0);
-  
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task])
+  }
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id))
+  }
+
   return (
-    <div className='App'>
-      <button
-        onClick={() => {
-          setNumber(number + 1);
-          localStorage.setItem('num', JSON.stringify(number+1))
-        }}
-      >
-        Increase count
-      </button>
-      <p>{number} </p>
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <div>
+              <h1>{task.taskName}</h1>
+              <button onClick={() => deleteTask(task.id)}>X</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
+  
 };
 
 
